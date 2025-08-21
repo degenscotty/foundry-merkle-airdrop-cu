6a8be5e0971dfc390cb2a230ecd213d493f76786
degenscotty <scottvermast@gmail.com>
2025-08-21 13:16:45 +0200

    edit: MerkleAirdrop.sol

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -9,6 +9,7 @@ contract MerkleAirdrop {
                                  ERRORS
     //////////////////////////////////////////////////////////////*/
     error MerkleAirdrop__InvalidProof();
+    error MerkleAirdrop__AlreadyClaimed();
 
     /*//////////////////////////////////////////////////////////////
                                  EVENTS
@@ -21,6 +22,7 @@ contract MerkleAirdrop {
     address[] claimers;
     bytes32 private immutable i_merkleRoot;
     IERC20 private immutable i_airdropToken;
+    mapping(address claimer => bool claimed) private s_hasClaimed;
 
     /*//////////////////////////////////////////////////////////////
                                  FUNCTIONS
@@ -31,14 +33,18 @@ contract MerkleAirdrop {
     }
 
     function claim(address account, uint256 amount, bytes32[] calldata merkleProof) external {
+        if (s_hasClaimed[account]) {
+            revert MerkleAirdrop__AlreadyClaimed();
+        }
+
         // calculate using the account and the amount, the hash -> leaf node
         bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encodePacked(account, amount))));
 
         if (!MerkleProof.verify(merkleProof, i_merkleRoot, leaf)) {
             revert MerkleAirdrop__InvalidProof();
         }
-
+        s_hasClaimed[account] = true;
         emit Claim(account, amount);
-        i_airdropToken.transfer(account, amount);
+        SafeERC20.safeTransfer(i_airdropToken, account, amount);
     }
 }
